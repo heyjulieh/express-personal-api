7795e6ff0d3cb4f2c4947c7dfc450bc69ad45829
heyjulieh <heyjulieh@gmail.com>
2017-04-13 14:22:40 -0700

    added MD file to link to in API

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get('/api', function apiIndex(req, res) {
   // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
   // It would be seriously overkill to save any of this to your database.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
+    areTheseAlltheEndPoints: true, // CHANGE ME ;)
+    message: "Welcome to my Julie's personal api website! Here's what you need to know!",
     documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
     baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
